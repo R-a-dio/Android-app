8005c8f25b9294b96e8838c0546c372528ef2a17
Rest Time <resttim3@gmail.com>
2013-07-18 20:02:46 -0500

    Searching auto complete progress

---
+++ src/src/io/radio/android/RadioAutoCompleteProvider.java
@@ -0,0 +1,47 @@
+package io.radio.android;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class RadioAutoCompleteProvider extends ContentProvider{
+
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri arg0, ContentValues arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] arg1, String arg2, String[] arg3,
+			String arg4) {
+		System.out.println(uri.getLastPathSegment());
+		return null;
+	}
+
+	@Override
+	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
