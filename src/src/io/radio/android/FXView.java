164263d77fa278e4dadbaae6991ded5c32d75987
Rest <resttim3@gmail.com>
2013-07-28 22:54:29 -0500

    One  little bar goes up and down

--- src/src/io/radio/android/FXView.java
+++ src/src/io/radio/android/FXView.java
@@ -2,12 +2,19 @@ package io.radio.android;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.media.audiofx.Visualizer;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class FXView extends View {
 	Visualizer visualizer;
+	static Rect bounds = new Rect();
+	byte[] fftData;
+
+	static Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 	public FXView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
@@ -20,11 +27,12 @@ public class FXView extends View {
 	public FXView(Context context) {
 		super(context);
 	}
-	
+
 	public void startFx(int playerId) {
 		visualizer = new Visualizer(playerId);
-		//visualizer.setCaptureSize(size);
-		//visualizer.setDataCaptureListener(listener, rate, waveform, fft);
+		visualizer.setCaptureSize(visualizer.getCaptureSizeRange()[1]);
+		visualizer.setDataCaptureListener(new onAudioData(),
+				Visualizer.getMaxCaptureRate(), false, true);
 		visualizer.setEnabled(true);
 	}
 
@@ -36,22 +44,34 @@ public class FXView extends View {
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-	}
-	
-	private class onAudioData implements Visualizer.OnDataCaptureListener
-    {
+		canvas.getClipBounds(bounds);
 
+		paint.setColor(Color.argb(128, 255, 255, 255));
+		paint.setStrokeWidth(10f);
+
+		if (fftData != null) {
+			byte rfk = fftData[0];
+			byte ifk = fftData[1];
+			float mag = (rfk * rfk + ifk * ifk);
+			int db = (int) (10 * Math.log10(mag));
+			// System.out.println("Mag: " + mag + " - " + "dB: " + db);
+			canvas.drawLine(bounds.centerX(), bounds.bottom, bounds.centerX(),
+					bounds.top + (db * 10), paint);
+		}
+	}
+
+	private class onAudioData implements Visualizer.OnDataCaptureListener {
 		public void onFftDataCapture(Visualizer visualizer, byte[] fft,
 				int samplingRate) {
-			// TODO Auto-generated method stub
-			
+			fftData = fft;
+			invalidate();
 		}
 
 		public void onWaveFormDataCapture(Visualizer visualizer,
 				byte[] waveform, int samplingRate) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
-    }
+
+	}
 }
